938cbd02d18d3aaebb1848b6d8aaab0fc60e3d46
Jarod Stewart <jarod@jarod.is>
2024-02-08 16:01:11 -0700

    Quickly extend chat route to call Ionic Commerce

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,6 +11,19 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+const queryIonic = async (query: string) => {
+  console.log({ query })
+  const res = await fetch('https://api.ioniccommerce.com/gpt/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: { query, num_results: 10 } })
+  })
+  const data = await res.json();
+  return JSON.stringify(data)
+}
+
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
@@ -26,15 +39,68 @@ export async function POST(req: Request) {
     openai.apiKey = previewToken
   }
 
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-0125",
+    messages: messages,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "query_ionic",
+          description: "Use this function to search for products and to get product recommendations.",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "A precise query of a product name or product category",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto", // auto is default, but we'll be explicit
+  });
+  const responseMessage = response.choices[0].message;
+
+  // Step 2: check if the model wanted to call a function
+  const toolCalls = responseMessage.tool_calls;
+  if (toolCalls) {
+    // Step 3: call the function
+    // Note: the JSON response may not always be valid; be sure to handle errors
+    const availableFunctions = {
+      query_ionic: queryIonic,
+    }; // only one function in this example, but you can have multiple
+    messages.push(responseMessage); // extend conversation with assistant's reply
+    for (const toolCall of toolCalls) {
+      const functionName = toolCall.function.name;
+      const functionToCall = availableFunctions[functionName];
+      const functionArgs = JSON.parse(toolCall.function.arguments);
+      console.log({ functionName, functionArgs })
+      const functionResponse = await functionToCall(functionArgs.query);
+      console.log({ functionResponse })
+      messages.push({
+        tool_call_id: toolCall.id,
+        role: "tool",
+        name: functionName,
+        content: functionResponse,
+      }); // extend conversation with function response
+    }
+  }
+
   const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-3.5-turbo-0125',
     messages,
     temperature: 0.7,
-    stream: true
+    stream: true,
   })
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
+      // const choice = json.choices[0]
+      // console.log({ choice })
       const title = json.messages[0].content.substring(0, 100)
       const id = json.id ?? nanoid()
       const createdAt = Date.now()
