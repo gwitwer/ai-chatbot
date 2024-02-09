0573d624bf5cf04cf0932a039b4777b6b10e750f
David Witwer <gdavid.witwer@gmail.com>
2024-02-09 15:59:14 -0500

    Local auth disable

--- auth.ts
+++ auth.ts
@@ -30,7 +30,7 @@ export const {
       return session
     },
     authorized({ auth }) {
-      return !!auth?.user // this ensures there is a logged in user for -every- request
+      return (process.env.NODE_ENV !== 'production' || !!auth?.user) // this ensures there is a logged in user for -every- request
     }
   },
   pages: {
