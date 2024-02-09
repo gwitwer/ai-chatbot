ba150bdfb3fafedf4c4a99666cfc5ecaacbfec3a
David Witwer <gdavid.witwer@gmail.com>
2024-02-09 16:18:56 -0500

    update auth redirecT?

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -25,7 +25,7 @@ export function LoginButton({
       onClick={() => {
         setIsLoading(true)
         // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-        signIn('github', { callbackUrl: `/api/auth` })
+        signIn('github', { callbackUrl: `${window.location.href}/api/auth` })
       }}
       disabled={isLoading}
       className={cn(className)}
